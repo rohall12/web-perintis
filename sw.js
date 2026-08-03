6c7debe7b76c26e19684bc2f7b8e891159ae1bcd
rohall12 <aiagentjerv@gmail.com>
2026-08-03 22:39:21 +0800

    Update PWA dan perbaikan service worker

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'ronihalla-pwa-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
